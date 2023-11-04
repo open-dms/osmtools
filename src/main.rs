818182601d293f650a676672c90ba97766d33cb1
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2023-11-03 22:13:40 -0300

    feat: add CLI features
    * add stats output
    * add line-json output

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,119 @@
-use std::{collections::HashMap, env::args};
+use std::{
+    collections::{BTreeMap, HashMap},
+    io::{stdout, BufWriter, Write},
+    path::PathBuf,
+};
 
-use anyhow::{anyhow, Result};
+use anyhow::{Ok, Result};
+use clap::{Parser, Subcommand};
+use itertools::Itertools;
+use osmpbfreader::{OsmId, OsmObj, OsmPbfReader};
+use serde_json::to_string;
+
+const TARGET_BOUNDARY_TYPES: &[&str] = &[
+    "administrative",
+    "state_border",
+    "country_border",
+    "state border",
+];
+
+#[derive(Parser)]
+#[command(name = "osmpbf-filter")]
+#[command(bin_name = "osmpbf-filter")]
+struct Cli {
+    #[arg(short, long)]
+    in_file: PathBuf,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Stats {},
+}
 
 fn main() -> Result<()> {
-    let path = args()
-        .nth(1)
-        .ok_or_else(|| anyhow!("expected input path as first argument"))?;
+    let cli = Cli::parse();
 
-    let f = std::fs::File::open(path)?;
+    eprintln!("Unpacking relations from {:?}", cli.in_file);
+    let relations: BTreeMap<OsmId, OsmObj>;
 
-    let mut pbf = osmpbfreader::OsmPbfReader::new(f);
-    let objs = pbf.get_objs_and_deps(|obj| {
-        obj.is_relation()
-            && obj.tags().contains("boundary", "administrative")
-            && obj.tags().contains_key("name")
-            && {
-                if let Some(admin_level) = obj.tags().get("admin_level") {
-                    admin_level == "2"
-                        || admin_level == "4"
-                        || admin_level == "6"
-                        || admin_level == "7"
-                        || admin_level == "8"
-                } else {
-                    false
-                }
-            }
-    })?;
-
-    let mut tags = HashMap::new();
-
-    for obj in objs.values() {
-        if obj.is_relation() {
-            // println!("{id:?}: {obj:?} {:?}", name);
-            let _ = obj
-                .tags()
-                .iter()
-                .filter(|(k, _)| !k.contains(':'))
-                .map(|(k, _)| {
-                    if let Some(tag) = tags.get_mut(k) {
-                        *tag += 1;
-                    } else {
-                        tags.insert(k, 1);
-                    }
-                })
-                .collect::<Vec<_>>();
+    match &cli.command {
+        Some(Commands::Stats {}) => {
+            relations = load_relations(cli.in_file, filter_target_relations)?;
+            eprintln!("Gathering some stats..");
+            to_stats(&relations);
+        }
+        None => {
+            relations = load_relations(cli.in_file, filter_all_relations)?;
+            to_jsonl(&relations);
         }
     }
 
-    dbg!(tags);
-
     Ok(())
+}
+
+fn filter_target_relations(obj: &OsmObj) -> bool {
+    filter_all_relations(obj)
+        && (obj.tags().get("boundary").map_or(false, |boundary| {
+            TARGET_BOUNDARY_TYPES.contains(&boundary.as_str())
+        }))
+}
+
+fn filter_all_relations(obj: &OsmObj) -> bool {
+    obj.is_relation() && obj.tags().contains_key("name") && {
+        if let Some(admin_level) = obj.tags().get("admin_level") {
+            admin_level == "2"
+                || admin_level == "4"
+                || admin_level == "6"
+                || admin_level == "7"
+                || admin_level == "8"
+        } else {
+            false
+        }
+    }
+}
+
+fn load_relations<F>(path: PathBuf, pred: F) -> Result<BTreeMap<osmpbfreader::OsmId, OsmObj>>
+where
+    F: FnMut(&OsmObj) -> bool,
+{
+    let f = std::fs::File::open(path)?;
+    let mut pbf = OsmPbfReader::new(f);
+    let relations = pbf.get_objs_and_deps(pred)?;
+    Ok(relations)
+}
+
+fn to_stats(relations: &BTreeMap<OsmId, OsmObj>) {
+    let mut boundary_types = HashMap::new();
+
+    for relation in relations.values().filter(|obj| filter_all_relations(obj)) {
+        let boundary = relation.tags().get("boundary");
+        if let Some(count) = boundary_types.get_mut(&boundary) {
+            *count += 1;
+        } else {
+            boundary_types.insert(boundary, 1);
+        }
+    }
+
+    for (boundary_type, count) in boundary_types.iter().sorted_by(|a, b| Ord::cmp(&b.1, &a.1)) {
+        if let Some(b_type) = boundary_type {
+            println!("{} {}", b_type, count);
+        }
+    }
+}
+
+fn to_jsonl(relations: &BTreeMap<OsmId, OsmObj>) {
+    let mut buffer = BufWriter::new(stdout());
+
+    for relation in relations
+        .values()
+        .filter(|obj| filter_target_relations(obj))
+    {
+        let serialized = to_string(&relation).unwrap();
+        writeln!(buffer, "{}", serialized).unwrap();
+    }
+
+    buffer.flush().unwrap();
 }
