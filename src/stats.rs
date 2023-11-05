c7d805f1785bdf31378de4670e05dcf898bc01d4
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2023-11-04 23:03:25 -0300

    add more stats

--- src/stats.rs
+++ src/stats.rs
@@ -1,29 +1,52 @@
 use anyhow::Result;
 use itertools::Itertools;
-use log::info;
 use osmpbfreader::OsmId;
 use osmpbfreader::OsmObj;
 use std::collections::BTreeMap;
 use std::collections::HashMap;
 use std::io;
+use std::vec::IntoIter;
 
 use crate::util::filter_all_relations;
 
+fn sort_count(map: HashMap<&str, usize>) -> IntoIter<(&str, usize)> {
+    map.into_iter()
+        .sorted_by(|a, b| Ord::cmp(&b.1, &a.1))
+        .collect::<Vec<_>>()
+        .into_iter()
+}
+
 pub fn to_stats(relations: BTreeMap<OsmId, OsmObj>, mut out: impl io::Write) -> Result<()> {
+    writeln!(out, "Stats\n{}", "-".repeat(20))?;
+    writeln!(out, "total relations: {}", relations.len())?;
+
     let mut boundary_types = HashMap::<&str, usize>::new();
+    let mut tags = HashMap::<&str, usize>::new();
 
-    info!("\n\nStats\n{}\nBoundary types (count):\n", "-".repeat(20));
+    for obj in relations.values().filter(|obj| filter_all_relations(obj)) {
+        if let Some(boundary) = obj.tags().get("boundary") {
+            *boundary_types.entry(boundary).or_default() += 1;
+        }
 
-    for boundary in relations
-        .values()
-        .filter(|obj| filter_all_relations(obj))
-        .filter_map(|obj| obj.tags().get("boundary"))
-    {
-        *boundary_types.entry(boundary).or_default() += 1;
+        for tag in obj
+            .tags()
+            .keys()
+            .filter_map(|tag| if tag != "boundary" { Some(tag) } else { None })
+        {
+            *tags.entry(tag).or_default() += 1;
+        }
     }
 
-    for (boundary_type, count) in boundary_types.iter().sorted_by(|a, b| Ord::cmp(&b.1, &a.1)) {
+    writeln!(out, "\nBoundary types (count):\n")?;
+
+    for (boundary_type, count) in sort_count(boundary_types) {
         writeln!(out, "{boundary_type} {count}")?;
+    }
+
+    writeln!(out, "\nOther tags (count):\n")?;
+
+    for (tag, count) in sort_count(tags) {
+        writeln!(out, "{tag} {count}")?;
     }
 
     Ok(())
