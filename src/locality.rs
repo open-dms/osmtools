f1eb478d2a6a4b6662f56708acd61fd8c4a73d46
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2023-11-06 11:58:09 -0300

    rename command endpoints

--- src/locality.rs
+++ src/locality.rs
@@ -8,15 +8,15 @@ use anyhow::Result;
 use osmpbfreader::{OsmId, OsmObj};
 use serde_json::to_string;
 
-use crate::util::filter_target_relations;
+use crate::util;
 
-pub fn to_jsonl(relations: BTreeMap<OsmId, OsmObj>, out: impl io::Write) -> Result<()> {
+pub fn write(relations: &BTreeMap<OsmId, OsmObj>, out: impl io::Write) -> Result<()> {
     // Use a buffered writer to amortize flushes.
     let mut buffer = BufWriter::new(out);
 
     for relation in relations
         .values()
-        .filter(|obj| filter_target_relations(obj))
+        .filter(|obj| util::filter_target_relations(obj))
     {
         let serialized = to_string(&relation)?;
         writeln!(buffer, "{serialized}")?;
