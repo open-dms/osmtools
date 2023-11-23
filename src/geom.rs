4b999c8b647fbd3fbcccba7a71568763cb6e08c3
Benjamin Bannier <bbannier@gmail.com>
2023-11-23 23:03:59 +0100

    Introduce dedicated Line type
    This type captures that lines consist of at least two points so they
    always have a defined start and end.

--- src/geom.rs
+++ src/geom.rs
@@ -24,6 +24,45 @@ impl Position {
     }
 }
 
+#[derive(Clone)]
+struct Line(Vec<Position>);
+
+impl Line {
+    fn start(&self) -> &Position {
+        self.0.first().expect("line cannot be empty")
+    }
+
+    fn end(&self) -> &Position {
+        self.0.last().expect("line cannot be empty")
+    }
+
+    fn extend(&mut self, tail: &Line) -> Result<()> {
+        if tail.start() == self.end() {
+            // If the current end matches the next start, extend normally
+            self.0.extend_from_slice(&tail.0[1..]);
+        } else if tail.end() == self.end() {
+            // If the current end matches the next end, extend in reverse
+            self.0.extend(tail.0.iter().rev().skip(1));
+        } else {
+            bail!("Linestrings do not form a continuous path");
+        }
+
+        Ok(())
+    }
+}
+
+impl TryFrom<Vec<Position>> for Line {
+    type Error = &'static str;
+
+    fn try_from(value: Vec<Position>) -> std::result::Result<Self, Self::Error> {
+        if value.len() < 2 {
+            return Err("cannot construct line with less than two points");
+        }
+
+        Ok(Self(value))
+    }
+}
+
 pub fn write(objs: &BTreeMap<OsmId, OsmObj>, out: impl io::Write) -> Result<()> {
     // Use a buffered writer to amortize flushes.
     let mut buffer = BufWriter::new(out);
@@ -113,6 +152,7 @@ fn as_polygon(obj: &OsmObj, all_objs: &BTreeMap<OsmId, OsmObj>) -> Result<geojso
                 None
             }
         })
+        .filter_map(|xs: Vec<_>| Line::try_from(xs).ok())
         .collect();
 
     // todo report missing geometry or broken linering
@@ -120,29 +160,27 @@ fn as_polygon(obj: &OsmObj, all_objs: &BTreeMap<OsmId, OsmObj>) -> Result<geojso
 
     // respect right hand rule
     if is_clockwise(&linering) {
-        linering.reverse();
+        linering.0.reverse();
     }
 
     Ok(geojson::Value::Polygon(vec![linering
+        .0
         .iter()
         .map(|p| vec![*p.0, *p.1])
         .collect()]))
 }
 
-/// create a continuous ring from line strings
-fn create_continuous_linering(linestrings: &Vec<Vec<Position>>) -> Result<Vec<Position>> {
-    if linestrings.is_empty() || linestrings.iter().any(|ls| ls.len() < 2) {
-        bail!("No linestrings or a linestring has less than 2 positions")
+/// Create a continuous ring from line strings.
+fn create_continuous_linering(linestrings: &Vec<Line>) -> Result<Line> {
+    if linestrings.is_empty() {
+        bail!("no linestrings")
     }
 
     // Convert the endpoint positions to a hashable type (tuple) and build the index map
     let mut endpoints: HashMap<Position, Vec<usize>> = HashMap::new();
     for (i, linestring) in linestrings.iter().enumerate() {
-        let start = Position::new(
-            *linestring.first().context("Empty linestring")?.0,
-            *linestring.first().unwrap().1,
-        );
-        let end = Position::new(*linestring.last().unwrap().0, *linestring.last().unwrap().1);
+        let start = Position::new(*linestring.start().0, *linestring.start().1);
+        let end = Position::new(*linestring.end().0, *linestring.end().1);
         endpoints.entry(start).or_default().push(i);
         if start != end {
             // Avoid double entry for loops
@@ -159,7 +197,7 @@ fn create_continuous_linering(linestrings: &Vec<Vec<Position>>) -> Result<Vec<Po
     used_linestrings.insert(first_index);
 
     while used_linestrings.len() < linestrings.len() {
-        let current_end_key = continuous_line.last().context("Empty line ring")?;
+        let current_end_key = continuous_line.end();
 
         let Some(indices) = endpoints.get(&current_end_key) else {
             bail!("No more matching linestrings found")
@@ -170,24 +208,13 @@ fn create_continuous_linering(linestrings: &Vec<Vec<Position>>) -> Result<Vec<Po
         };
 
         let next_linestring = &linestrings[next_index];
-        let next_start_key = Position::new(*next_linestring[0].0, *next_linestring[0].1);
-        let next_end_key = next_linestring.last().context("Next linestring empty")?;
-
-        if current_end_key == &next_start_key {
-            // If the current end matches the next start, extend normally
-            continuous_line.extend_from_slice(&next_linestring[1..]);
-        } else if current_end_key == next_end_key {
-            // If the current end matches the next end, extend in reverse
-            continuous_line.extend(next_linestring[..next_linestring.len() - 1].iter().rev());
-        } else {
-            bail!("Linestrings do not form a continuous path");
-        }
+        continuous_line.extend(next_linestring)?;
 
         used_linestrings.insert(next_index);
     }
 
     // Check if the start and end positions match to close the loop
-    if continuous_line.first() != continuous_line.last() {
+    if continuous_line.start() != continuous_line.end() {
         bail!("Ends of the linestrings don't form a ring");
     }
 
@@ -195,11 +222,11 @@ fn create_continuous_linering(linestrings: &Vec<Vec<Position>>) -> Result<Vec<Po
 }
 
 /// Calculate the orientation of the ring
-fn is_clockwise(ring: &[Position]) -> bool {
+fn is_clockwise(ring: &Line) -> bool {
     // Calculate the signed area under the curve (Shoelace formula).
 
-    let cur = ring.iter();
-    let next = ring.iter().chain(ring.iter()).skip(1);
+    let cur = ring.0.iter();
+    let next = ring.0.iter().chain(ring.0.iter()).skip(1);
     cur.zip(next)
         .map(|(c, n)| *((n.0 - c.0) * (n.1 + c.1)))
         .sum::<f64>()
@@ -210,7 +237,7 @@ fn is_clockwise(ring: &[Position]) -> bool {
 mod test {
     #[test]
     fn is_clockwise() {
-        use super::{is_clockwise, Position};
+        use super::{is_clockwise, Line, Position};
 
         // Points in the four quadrants of a cartesian coordinate system. Numbering here is
         // counterclockwise.
@@ -219,15 +246,13 @@ mod test {
         let q3 = Position::new(-1.0, -1.0);
         let q4 = Position::new(-1.0, 1.0);
 
-        // Degenerate cases.
-        assert!(!is_clockwise(&[]));
-        assert!(!is_clockwise(&vec![q1]));
-
         // Segments with two elements are both clock- and counterclockwise.
-        assert!(!is_clockwise(&vec![q1, q2]));
-        assert!(!is_clockwise(&vec![q2, q1]));
+        assert!(!is_clockwise(&Line::try_from(vec![q1, q2]).unwrap()));
+        assert!(!is_clockwise(&Line::try_from(vec![q2, q1]).unwrap()));
 
-        assert!(is_clockwise(&vec![q1, q2, q3, q4]));
-        assert!(!is_clockwise(&vec![q4, q3, q2, q1]));
+        assert!(is_clockwise(&Line::try_from(vec![q1, q2, q3, q4]).unwrap()));
+        assert!(!is_clockwise(
+            &Line::try_from(vec![q4, q3, q2, q1]).unwrap()
+        ));
     }
 }
